053988cd872318462b61959cdf5c4c7971abbf7f
lazygyu <lazygyu@gmail.com>
2022-10-12 23:42:00 +0900

    add the center parameter to the createMover

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 import * as planck from 'planck';
 
-export function createMover(world: planck.World, pos: planck.Vec2, power: number) {
-    const mover = world.createKinematicBody({position: pos});
+export function createMover(world: planck.World, pos: planck.Vec2, power: number, center?: planck.Vec2) {
+    const mover = world.createKinematicBody({position: pos });
     mover.createFixture({
-        shape: new planck.Box(2, 0.1),
+        shape: new planck.Box(2, 0.1, center ? center : new planck.Vec2()),
         restitution: 0.7,
     });
     mover.setAngularVelocity(power);
