8f0d887e9ef1d05b3cb9c47315bea70c60ec7d15
lazygyu <lazygyu@gmail.com>
2024-07-03 03:42:50 +0900

    localization

---
+++ src/data/languages.ts
@@ -0,0 +1,40 @@
+export const Translations = {
+    'en': {
+        'Enter names below': 'Enter names below',
+        'Shuffle': 'Shuffle',
+        'Start': 'Start',
+        'Map': 'Map',
+        'Recording': 'Recording',
+        'The winner is': 'The winner is',
+        'Using skills': 'Using skills',
+        'Buy me a coffee': 'Buy me a coffee',
+        'First': 'First',
+        'Last': 'Last',
+        'Wheel of fortune': 'Wheel of fortune',
+        'BubblePop': 'BubblePop',
+        'Pot of greed': 'Pot of greed',
+        'Shake!': 'Shake!',
+        'Input names separated by commas or line feed here': 'Input names separated by commas or line feed here',
+    },
+    'ko': {
+        'Enter names below': '이름들을 입력하세요',
+        'Shuffle': '섞기',
+        'Start': '시작',
+        'Map': '맵',
+        'Recording': '녹화',
+        'The winner is': '당첨 순위',
+        'Using skills': '스킬 활성화',
+        'Buy me a coffee': '개발자에게 커피 사주기',
+        'First': '첫번째',
+        'Last': '마지막',
+        'Wheel of fortune': '운명의 수레바퀴',
+        'BubblePop': '버블팝',
+        'Pot of greed': '욕망의 항아리',
+        'Shake!': '흔들기!',
+        'Input names separated by commas or line feed here': '이름들을 쉼표나 엔터로 구분해서 넣어주세요',
+    },
+} as const;
+
+export type TranslatedLanguages = keyof typeof Translations;
+
+export type TranslationKeys = keyof typeof Translations[TranslatedLanguages];
---
+++ src/localization.ts
@@ -0,0 +1,51 @@
+import {TranslatedLanguages, TranslationKeys, Translations} from './data/languages';
+
+const defaultLocale: TranslatedLanguages = 'en';
+let locale: TranslatedLanguages | undefined;
+
+function getBrowserLocale() {
+    return navigator.language.split('-')[0];
+}
+
+function translateElement(element: Element) {
+    if (!(element instanceof HTMLElement) || !locale) return;
+
+    const prop = element.getAttribute('data-trans');
+
+
+    if (prop) {
+        const key = (element.getAttribute(prop) || '').trim();
+        if (key && key in Translations[locale]) {
+            element.setAttribute(prop, Translations[locale][key as TranslationKeys]);
+        }
+    } else {
+        const key = element.innerText.trim();
+        if (key && key in Translations[locale]) {
+            element.innerText = Translations[locale][key as TranslationKeys];
+        }
+    }
+}
+
+function translatePage() {
+    document.querySelectorAll('[data-trans]').forEach(translateElement);
+}
+
+function setLocale(newLocale: string) {
+   if (newLocale === locale) return;
+
+   document.documentElement.lang = newLocale;
+
+   const newLocaleLower = newLocale.toLocaleLowerCase();
+
+   locale = newLocaleLower in Translations ? (newLocaleLower as TranslatedLanguages) : defaultLocale;
+   translatePage();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('localization loaded');
+    const browserLocale = getBrowserLocale();
+    console.log('detected locale: ', browserLocale);
+    setLocale(browserLocale);
+});
+
+(window as any).translateElement = translateElement;
