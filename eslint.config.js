17cf7c79ba4344c10b071553dac0fd5a34e16e64
lazygyu <lazygyu@gmail.com>
2024-11-23 03:22:00 +0900

    introduce a linter

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import typescriptEslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintCongigPrettier from 'eslint-config-prettier';
+
+export default typescriptEslint.config(typescriptEslint.configs.recommended, {
+  files: ['**/*.ts'],
+  // Prettier 규칙을 ESLint 통합
+  plugins: { prettier: eslintPluginPrettier },
+  extends: [
+    eslintCongigPrettier, // ESLint와 Prettier 충돌 방지
+  ],
+  rules: {
+    '@typescript-eslint/no-unsafe-function-type': 'off',
+    // Prettier 규칙을 ESLint 에러로 표시
+    'prettier/prettier': 'error',
+  },
+});
