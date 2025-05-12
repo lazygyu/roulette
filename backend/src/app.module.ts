3da0a5e347f4fac68089ef748f9131fdc26a98a0
leejs1030 <leejs1030@korea.ac.kr>
2025-05-12 14:05:18 +0900

    interceptor/pipe 적용

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,37 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ClassSerializerInterceptor, Module, ValidationPipe } from '@nestjs/common';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { GameModule } from './game/game.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { APP_INTERCEPTOR, APP_PIPE, Reflector } from '@nestjs/core';
 
 @Module({
   imports: [UsersModule, AuthModule, RoomsModule, GameModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useFactory: (reflector: Reflector) => {
+        return new ClassSerializerInterceptor(reflector, {
+          strategy: 'excludeAll',
+          exposeDefaultValues: true,
+        });
+      },
+      inject: [Reflector],
+    },
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    },
+  ],
 })
 export class AppModule {}
