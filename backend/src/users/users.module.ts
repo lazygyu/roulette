ceb963780c475a706438f3207e48bcc209152330
leejs1030 <leejs1030@korea.ac.kr>
2025-05-12 13:54:27 +0900

    PrismaModule 사용

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService],
+  imports: [PrismaModule],
   exports: [UsersService],
 })
 export class UsersModule {}
