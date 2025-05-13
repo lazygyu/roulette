063aba70a91c4584e83062f16b3ea71da2b69097
leejs1030 <leejs1030@korea.ac.kr>
2025-05-13 16:40:30 +0900

    응답 dto 수정

--- backend/src/game/dto/get-game-ranking-response.dto.ts
+++ backend/src/game/dto/get-game-ranking-response.dto.ts
@@ -1,5 +1,8 @@
+import { Expose, Type } from 'class-transformer';
 import { GameRankingEntryDto } from './game-ranking-entry.dto';
 
 export class GetGameRankingResponseDto {
+  @Expose()
+  @Type(() => GameRankingEntryDto)
   rankings: GameRankingEntryDto[];
 }
