b34e96940f7eaece8029056047a0bb7a7e832efc
leejs1030 <leejs1030@korea.ac.kr>
2025-05-21 16:54:49 +0900

    strict 모드 부활

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,4 +4,8 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
