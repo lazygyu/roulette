5174c7ea9f0b91d819b383efbdcedcf200ea7c5e
leejs1030 <leejs1030@korea.ac.kr>
2025-06-07 15:57:20 +0900

    미사용 파일 제거

--- frontend/src/options.ts
+++
@@ -1,9 +0,0 @@
-class Options {
-  useSkills: boolean = true;
-  winningRank: number = 0;
-  autoRecording: boolean = true;
-  speed: number = 1; // speed 속성 추가
-}
-
-const options = new Options();
-export default options;
