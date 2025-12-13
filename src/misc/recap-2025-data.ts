88b41993b6c6ccbe7615d2f0385e841c569e0f79
lazygyu <lazygyu@gmail.com>
2025-12-14 05:43:47 +0900

    WIP

--- src/misc/recap-2025-data.ts
+++ src/misc/recap-2025-data.ts
@@ -3,7 +3,7 @@ export const data = {
   people: 1818504,
   startCount: 8959466,
   busiestTime: { from: 12, to: 13 },
-  marbleCount: 1178628773,
+  marbleCount: 1193837128,
   country: 147,
 
 } as const;
