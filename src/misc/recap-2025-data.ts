059bea9ac43a93b945791586c6b7de8565cb87c4
lazygyu <lazygyu@gmail.com>
2026-01-02 01:43:57 +0900

    update recap data

--- src/misc/recap-2025-data.ts
+++ src/misc/recap-2025-data.ts
@@ -1,12 +1,12 @@
 export const data = {
-  date: '2025년 12월 29일',
-  people: 1901495,
-  startCount: 9479112,
+  date: '2025년 12월 31일',
+  people: 1910547,
+  startCount: 9542013,
   busiestTimeFrom: 12,
   busiestTimeTo: 13,
   busiestDateMonth: 11,
   busiestDateDate: 11,
-  marbleCount: 1298856408,
+  marbleCount: 1308480386,
   country: 147,
   coffeeBuyer: 37,
 } as const;
